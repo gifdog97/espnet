88b557cdf3ce116d06ab183949cba061962f06a0
gifdog97 <omusubset.s@gmail.com>
2026-02-24 14:16:50 +0900

    speech up summarize pairwise result

--- egs2/ljspeech/tts1/myscripts/summarize_pairwise_result.py
+++ egs2/ljspeech/tts1/myscripts/summarize_pairwise_result.py
@@ -1,11 +1,29 @@
+import argparse
+import os
 import re
+from multiprocessing import Pool
 from pathlib import Path
 
-PAIRWISE_DIR = Path("pairwise")
+from tqdm import tqdm
+
+PAIRWISE_DIR = Path("pairwise_10s")
 SCORE_MAP = {"A>>B": 1, "A>B": 0.5, "A=B": 0, "B>A": -0.5, "B>>A": -1}
 
 
-def parse_llm_output(llm_output_path: Path, setting_X: str) -> str:
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "--pairwise_dir",
+        type=Path,
+        default=PAIRWISE_DIR,
+        help="Directory containing pairwise LLM evaluation results",
+    )
+    return parser.parse_args()
+
+
+def parse_llm_output(
+    llm_output_path: Path, setting_X: str
+) -> tuple[float, int, int, int]:
     with llm_output_path.open("r") as f:
         content = [line.rstrip("\n") for line in f if line.strip()]
     # Text A (vits-20-128-0.7) -> vits-20-128-0.7
@@ -19,38 +37,60 @@ def parse_llm_output(llm_output_path: Path, setting_X: str) -> str:
         score = SCORE_MAP[judge]
     else:  # A and B is swapped
         score = -SCORE_MAP[judge]
-    return score
+    text_A_length = len(content[1].strip().split(" "))
+    text_B_length = len(content[3].strip().split(" "))
+    length_diff = text_A_length - text_B_length
+    return score, text_A_length, text_B_length, length_diff
 
 
 def write_summary(pairwise_dir: Path):
     setting_X, _ = pairwise_dir.name.split("_vs_")
-    summary_lines = []
+    summary_dict = {
+        "utt_id": [],
+        "score": [],
+        "length_A": [],
+        "length_B": [],
+        "length_diff": [],
+    }
     for llm_output in pairwise_dir.iterdir():
+        if "summary" in llm_output.name:
+            continue
         if not llm_output.name.endswith(".txt"):
             continue
         utt_id = llm_output.stem
         try:
-            judge = parse_llm_output(llm_output, setting_X)
+            score, text_A_length, text_B_length, length_diff = parse_llm_output(
+                llm_output, setting_X
+            )
         except ValueError as e:
             print(e)
             continue
-        summary_lines.append(f"{utt_id},{judge}")
+        summary_dict["utt_id"].append(utt_id)
+        summary_dict["score"].append(score)
+        summary_dict["length_A"].append(text_A_length)
+        summary_dict["length_B"].append(text_B_length)
+        summary_dict["length_diff"].append(length_diff)
     summary_path = pairwise_dir / "summary.txt"
     with summary_path.open("w") as f:
-        f.write("utt_id,score\n")
-        for line in summary_lines:
-            f.write(line + "\n")
+        f.write("utt_id,score,length_A,length_B,length_diff\n")
+        for i in range(len(summary_dict["utt_id"])):
+            f.write(
+                f"{summary_dict['utt_id'][i]},{summary_dict['score'][i]},{summary_dict['length_A'][i]},{summary_dict['length_B'][i]},{summary_dict['length_diff'][i]}\n"
+            )
+    return summary_dict
 
 
 def main():
-    for pairwise_dir in PAIRWISE_DIR.iterdir():
+    args = parse_args()
+    cpu_count = os.cpu_count()
+    pairwise_dirs = []
+    for pairwise_dir in Path(args.pairwise_dir).iterdir():
         if "_vs_" not in pairwise_dir.name:
             continue
-        if (pairwise_dir / "summary.txt").exists():
-            print(f"summary.txt already exists in {pairwise_dir}, skipping...")
-            continue
-        print(pairwise_dir)
-        write_summary(pairwise_dir)
+        pairwise_dirs.append(pairwise_dir)
+    with Pool(cpu_count) as pool, tqdm(total=len(pairwise_dirs)) as pbar:
+        for _ in pool.imap(write_summary, pairwise_dirs):
+            pbar.update(1)
 
 
 if __name__ == "__main__":
