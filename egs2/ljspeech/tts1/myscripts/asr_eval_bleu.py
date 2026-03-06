afee955caa60ca8056653d69d16b3e0011dd3c13
gifdog97 <omusubset.s@gmail.com>
2026-03-06 15:53:26 +0900

    update correlation analysis

--- egs2/ljspeech/tts1/myscripts/asr_eval_bleu.py
+++ egs2/ljspeech/tts1/myscripts/asr_eval_bleu.py
@@ -42,16 +42,20 @@ def get_self_bleu(utterances, averaging_mode, weights):
     self_bleu = []
 
     for i in range(len(utterances)):
-        hypo = utterances[i]
-        rest = utterances[:i] + utterances[i + 1 :]
+        utt_id = utterances[i][0]
+        hypo = utterances[i][1]
+        rest = [u[1] for u in utterances[:i]] + [u[1] for u in utterances[i + 1 :]]
 
         self_bleu.append(
-            sentence_bleu(
-                rest,
-                hypo,
-                weights,
-                no_length_penalty=True,
-                averaging_mode=averaging_mode,
+            (
+                utt_id,
+                sentence_bleu(
+                    rest,
+                    hypo,
+                    weights,
+                    no_length_penalty=True,
+                    averaging_mode=averaging_mode,
+                ),
             )
         )
 
@@ -99,17 +103,18 @@ def get_self_bleu3_geometric(utterances):
 
 
 def auto_bleu(sentence, weights, mean_mode="arithmetic"):
-    if len(sentence) <= 1:
+    if len(sentence[1]) <= 1:
         return 0
+    utt_id = sentence[0]
 
     N = len(weights)
 
     bleu_n = np.zeros([N])
     for n in range(N):
-        targ_ngrams = list(nltk.ngrams(sentence, n + 1))
+        targ_ngrams = list(nltk.ngrams(sentence[1], n + 1))
         for p in range(len(targ_ngrams)):
-            left = sentence[:p]
-            right = sentence[(p + n + 1) :]
+            left = sentence[1][:p]
+            right = sentence[1][(p + n + 1) :]
             rest_ngrams = list(nltk.ngrams(left, n + 1)) + list(
                 nltk.ngrams(right, n + 1)
             )
@@ -119,9 +124,9 @@ def auto_bleu(sentence, weights, mean_mode="arithmetic"):
 
     weights = np.array(weights)
     if mean_mode == "arithmetic":
-        return (bleu_n * weights).sum()
+        return (utt_id, (bleu_n * weights).sum())
     elif mean_mode == "geometric":
-        return (bleu_n**weights).prod()
+        return (utt_id, (bleu_n**weights).prod())
     else:
         raise ValueError(f"Unknown agggregation mode {mean_mode}")
 
@@ -130,7 +135,7 @@ def all_scores(asr_transcript):
     with open(asr_transcript, "r") as fin:
         lines = fin.readlines()
 
-    terms = [x.strip().split() for x in lines]
+    terms = [x.strip().split("|") for x in lines]
 
     tasks = [
         # ("Self-BLEU2-arithmetic", get_self_bleu2_arithmetic),
@@ -145,17 +150,19 @@ def all_scores(asr_transcript):
 
     n_processes = min(16, len(tasks))
     with Pool(n_processes) as pool:
-        metrics = pool.map(run_f, [(t[1], terms) for t in tasks])
+        id_and_metrics = pool.map(run_f, [(t[1], terms) for t in tasks])
 
     metric_dict = defaultdict(list)
-    for (metric_name, _), metric in zip(tasks, metrics):
-        for m in metric:
-            metric_dict[metric_name].append(m)
+    for (metric_name, _), id_and_metric in zip(tasks, id_and_metrics):
+        for wav_id, m in id_and_metric:
+            metric_dict[metric_name].append((wav_id, m))
         # metric, sem = np.mean(metric), np.std(metric) / np.sqrt(len(metric))
         # metric, sem = [round(100 * x, 2) for x in [metric, sem]]
 
-    for m1, m2 in zip(metrics[0], metrics[1]):
-        metric_dict["VERT"].append(round(np.sqrt(100 * m1 * 100 * m2), 2))
+    for m1, m2 in zip(metric_dict[tasks[0][0]], metric_dict[tasks[1][0]]):
+        metric_dict["VERT"].append(
+            (m1[0], round(np.sqrt(100 * m1[1] * 100 * m2[1]), 2))
+        )
     # vert = round(np.sqrt(100 * np.mean(metrics[0]) * 100 * np.mean(metrics[1])), 2)
 
     return metric_dict
@@ -167,7 +174,7 @@ def main():
     with open(args.asr_transcript, "r") as fin:
         lines = fin.readlines()
 
-    terms = [x.strip().split() for x in lines]
+    terms = [x.strip().split("|") for x in lines]
 
     if args.debug:
         terms = terms[:10]
@@ -185,15 +192,24 @@ def main():
 
     n_processes = min(16, len(tasks))
     with Pool(n_processes) as pool:
-        metrics = pool.map(run_f, [(t[1], terms) for t in tasks])
+        id_and_metrics = pool.map(run_f, [(t[1], terms) for t in tasks])
 
     with open(args.output_file, "w") as f:
-        for (metric_name, _), metric in zip(tasks, metrics):
-            metric, sem = np.mean(metric), np.std(metric) / np.sqrt(len(metric))
+        for (metric_name, _), id_and_metric in zip(tasks, id_and_metrics):
+            metrics = [x[1] for x in id_and_metric]
+            metric, sem = np.mean(metrics), np.std(metrics) / np.sqrt(len(metrics))
             metric, sem = [round(100 * x, 2) for x in [metric, sem]]
             f.write(f"{metric_name} {metric}+-{sem}\n")
 
-        vert = round(np.sqrt(100 * np.mean(metrics[0]) * 100 * np.mean(metrics[1])), 2)
+        vert = round(
+            np.sqrt(
+                100
+                * np.mean([x[1] for x in id_and_metrics[0]])
+                * 100
+                * np.mean([x[1] for x in id_and_metrics[1]])
+            ),
+            2,
+        )
         f.write(f"VERT {vert}")
 
 
